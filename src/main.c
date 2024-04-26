d96b3aaf223e3cb0d870b58830190dcf6d85a101
wallacegibbon <wallacegibbon@aliyun.com>
2024-04-26 14:52:38 +0800

    Trivial changes: Interesting code to demonstrate pointers of different size. (__code, __xdata, __pdata, __idata, __data)

--- src/main.c
+++ src/main.c
@@ -1,9 +1,21 @@
 #include "warning.h"
 #include <mcs51/8051.h>
 
+__data unsigned char *d1;
+__idata unsigned char *d2;
+__pdata unsigned char *d3;
+__xdata unsigned char *d4;
+__code unsigned char *d5;
+unsigned char *d6;
+
 void main(void)
 {
 	P1_1 = 0;
-	P2_1 = 1;
+	P2 = sizeof(d1);
+	P2 = sizeof(d2);
+	P2 = sizeof(d3);
+	P2 = sizeof(d4);
+	P2 = sizeof(d5);
+	P2 = sizeof(d6);
 	blah();
 }
